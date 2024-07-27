54851471ad96e0b4f7128f0bcf1a61d7d4a42344
marcosht <marcoshealthtraining@gmail.com>
2024-07-27 15:01:22 -0400

    Update certificates.js

--- data/certificates.js
+++ data/certificates.js
@@ -1,5 +1,6 @@
 const certificates = [
-    { number: '12345', course: 'AHA BLS', owner: 'Juan Perez', issueDate: '2023-01-15', expirationDate: '2025-01-15' },
+    { number: '12345', course: 'AHA BLS', owner: 'Juan Perez', issueDate: '1/15/2023', expirationDate: '1/15/2025', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+    { number: '12346', course: 'Phlebotomy Technician', owner: 'Maria Garcia', issueDate: '1/15/2024', expirationDate: '1/15/2026', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
     { number: '67890', course: 'AHA BLS', owner: 'Maria Garcia', issueDate: '2022-05-20', expirationDate: '2024-05-20' },
     { number: '11223', course: 'AHA BLS', owner: 'Carlos Rodriguez', issueDate: '2021-08-30', expirationDate: '2023-08-30' },
     // Agrega más certificados según sea necesario
