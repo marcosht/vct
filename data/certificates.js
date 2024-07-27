1429f0735cf056b08b8561ff8e0301f6d0e8fa6d
marcosht <marcoshealthtraining@gmail.com>
2024-07-27 15:42:00 -0400

    Update certificates.js

--- data/certificates.js
+++ data/certificates.js
@@ -1,8 +1,13 @@
 const certificates = [
-    { number: '12345', course: 'AHA BLS', owner: 'Juan Perez', issueDate: '1/15/2023', expirationDate: '1/15/2025', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
-    { number: '12346', course: 'Phlebotomy Technician', owner: 'Maria Garcia', issueDate: '1/15/2024', expirationDate: '1/15/2026', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
-    { number: '67890', course: 'AHA BLS', owner: 'Maria Garcia', issueDate: '2022-05-20', expirationDate: '2024-05-20' },
-    { number: '11223', course: 'AHA BLS', owner: 'Carlos Rodriguez', issueDate: '2021-08-30', expirationDate: '2023-08-30' },
+{ number: '12345', course: 'BLS', owner: 'Juan Perez', issueDate: '1/15/2023', expirationDate: '1/15/2025', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '12346', course: 'Phlebotomy Technician', owner: 'Maria Garcia', issueDate: '1/15/2024', expirationDate: 'No expiration', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '255421509218', course: 'ACLS', owner: 'DORALBA DAVID', issueDate: '7/29/2024', expirationDate: '7/29/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '255411509219', course: 'BLS', owner: 'DORALBA DAVID', issueDate: '7/29/2024', expirationDate: '7/29/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '255401509215', course: 'ACLS', owner: 'DORALBA DAVID', issueDate: '7/29/2024', expirationDate: '7/29/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '255421152625', course: 'PALS', owner: 'WILSON AGUINALDO', issueDate: '7/7/2024', expirationDate: '7/7/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '255401152607', course: 'ACLS', owner: 'WILSON AGUINALDO', issueDate: '7/6/2024', expirationDate: '7/6/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '255411152563', course: 'BLS', owner: 'WILSON AGUINALDO', issueDate: '7/6/2024', expirationDate: '7/6/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '255411152562', course: 'BLS', owner: 'CARLOS FEBUS', issueDate: '7/6/2024', expirationDate: '7/6/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
     // Agrega más certificados según sea necesario
 ];
 
