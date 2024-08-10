8114e0f933f3459c1f90a9169df924c5b55cd19c
marcosht <marcoshealthtraining@gmail.com>
2024-08-09 22:40:51 -0400

    Update certificates.js

--- data/certificates.js
+++ data/certificates.js
@@ -8,6 +8,16 @@ const certificates = [
 { number: '255401152607', course: 'ACLS', owner: 'WILSON AGUINALDO', issueDate: '7/6/2024', expirationDate: '7/6/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
 { number: '255411152563', course: 'BLS', owner: 'WILSON AGUINALDO', issueDate: '7/6/2024', expirationDate: '7/6/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
 { number: '255411152562', course: 'BLS', owner: 'CARLOS FEBUS', issueDate: '7/6/2024', expirationDate: '7/6/2026', imageUrl: 'https://marcosht.github.io/vct/img/aha.jpg' },
+{ number: '168124801', course: 'Phlebotomy Technician', owner: 'CLARA ALVEAR', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '168124802', course: 'Phlebotomy Technician', owner: 'KEVIN VALLEJOS ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '168124803', course: 'Phlebotomy Technician', owner: 'MARIA FIGUEROA ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '168124804', course: 'Phlebotomy Technician', owner: 'KAMILLE BELL ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '168124805', course: 'Phlebotomy Technician', owner: 'DARLENE VANDERHORST ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '511724801', course: 'EKG Technician', owner: 'CLARA ALVEAR ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '511724802', course: 'EKG Technician', owner: 'KEVIN VALLEJOS ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '511724803', course: 'EKG Technician', owner: 'MARIA FIGUEROA ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '511724804', course: 'EKG Technician', owner: 'KAMILLE BELL ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
+{ number: '511724805', course: 'EKG Technician', owner: 'DARLENE VANDERHORST ', issueDate: '8/11/2024', expirationDate: 'N/A', imageUrl: 'https://marcosht.github.io/vct/img/marcos.jpg' },
     // Agrega más certificados según sea necesario
 ];
 
